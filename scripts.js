1cc895b38d00750fb147626ee5976130cf9bf325
HarryStevens <harryjosephstevens@gmail.com>
2014-03-31 16:26:17 -0400

    all works but show active

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
 /**
  * @author Harry Stevens
  */
+
+function setNav() {
+
+	$(".button").on("click",function() {
+		
+		//Something happens when .button elements are clicked
+				
+		var buttonName = $(this).attr("ref");
+		
+		if ($(this).hasClass("active")) {
+			//do nothing
+		} else {
+			$("img.active").removeClass("active").fadeOut();
+			$("img."+buttonName).addClass("active").fadeIn();
+			
+			$(".button.active").removeClass("active");
+			$(this).addClass("active");
+		}
+$("img.active").show();
+
+		
+	});
+	
+	 
+}
+
+
+
+$(document).ready(function(){
+
+	setNav();
+});
